29fd505f14a9cd1a636dd412374f48bdc0cea2e8
RuthMery <ruth.cardenas.perez@gmail.com>
2017-06-09 11:35:26 -0500

    Javascript-Nombres de las coders

---
+++ js/chile.js
@@ -0,0 +1,22 @@
+var mexico = [{
+              imagen: "Anais Araya.JPG",
+              name: "Anais"
+            },{
+              image: "Andrea Miranda.JPG",
+              name: "Andrea"
+            },{
+              image: "Berenisse Ríos.JPG",
+              name: "Berenisse"
+            },{
+              image: "Caterina da Silva.JPG",
+              name: "Caterina"
+            },{
+              image: "Daniela Sanchez.JPG",
+              name: "Daniela"
+            },{
+              image: "Francisca Ojeda.JPG",
+              name: "Francisca"
+            },{
+              image: "Katerine Sandoval.JPG",
+              name: "Katerine"
+            }];
