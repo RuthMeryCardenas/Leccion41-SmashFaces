7b575376c38d9fdcbc1446f45393e9eb5277c7a5
RuthMery <ruth.cardenas.perez@gmail.com>
2017-06-12 12:51:00 -0500

    Funcionalidad a seleccionar sede

---
+++ js/components/photo.js
@@ -0,0 +1,32 @@
+var photo_container = $(".photo");
+var reRender = function () {
+  photo_container.empty();
+}
+var Photo_coder = function (seat, photo_url) {
+  photo_container.empty();
+  var static_url = "assets/img/";
+  var img = $("<img alt='Foto de coder'>");
+  img.attr("src", static_url + seat + "/" + photo_url);
+  photo_container.append(img);
+  // return img;
+}
+var generate_random_numbers = function (array) {
+  var aux_arr = [];
+  var total_elements = 5; // Cantidad de números en el arreglo
+  var end_range = array.length; //Máximo valor de los números en el arreglo
+  while(aux_arr.length < total_elements && total_elements < end_range){
+    var ramdom_number = Math.floor(Math.random() * end_range);
+    if(!aux_arr.some(function(number){return number == ramdom_number})){
+        aux_arr.push(ramdom_number);
+    }
+  }
+  return aux_arr;
+}
+var load_random_photos = function (current_coder, answer) {
+  console.log(current_coder.name);
+  if (answer == current_coder.name) {
+    console.log("ok");
+  }else {
+    console.log("sigue intentando");
+  }
+}
--- js/index.js
+++ js/index.js
@@ -7,6 +7,40 @@ $(document).ready(function () {
   getJSON('lima.json', function (err, json) {
     if (err) { return alert(err.message);}
     coders.lima = json;
-    console.log(coders.lima[0]);
+  });
+  getJSON('chile.json', function (err, json) {
+    if (err) { return alert(err.message);}
+    coders.chile = json;
+  });
+  getJSON('mexico.json', function (err, json) {
+    if (err) { return alert(err.message);}
+    coders.mexico = json;
+  });
+  $(".seat select").on("change", function () {
+    var current_seat = $(".seat select").val();
+    switch (current_seat) {
+      case "lima":
+        var random_numbers = generate_random_numbers(coders.lima);
+        var accountant = 0;
+        var current_coder = coders.lima[random_numbers[accountant]];
+        Photo_coder(current_seat, current_coder.image);
+        $(".to-play button").on("click", load_random_photos (current_coder, $(".to-play input").val()));
+        break;
+      case "arequipa":
+        console.log("no data");
+        break;
+      case "chile":
+        $(".to-play button").on("click", function () {
+          console.log("hola chile");
+        })
+        break;
+      case "mexico":
+      $(".to-play button").on("click", function () {
+        console.log("hola mexico");
+      })
+        break;
+      default:
+        console.log("Debe elegir una sede");
+    }
   });
 });
